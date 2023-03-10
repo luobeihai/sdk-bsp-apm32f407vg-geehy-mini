9b85f8ac06fd10c99b3fdcc1318f71e2431cf6c0
luobeihai <571688853@qq.com>
2023-03-10 13:24:00 +0800

    fix some bug and update rtt driver

--- project_0/rtconfig_preinc.h
+++ project_0/rtconfig_preinc.h
@@ -6,9 +6,8 @@
 /* RT-Thread pre-include file */
 
 #define APM32F40X
-#define HAVE_CCONFIG_H
 #define RT_USING_LIBC
-#define RT_USING_NEWLIB
+#define RT_USING_NEWLIBC
 #define USE_STDPERIPH_DRIVER
 #define _POSIX_C_SOURCE 1
 #define __RTTHREAD__
